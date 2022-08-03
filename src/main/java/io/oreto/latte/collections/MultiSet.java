e425fd2b25b4e8f54cbab8279b3966cfd6c5b774
ross-oreto <ross.oreto@gmail.com>
2022-08-03 11:42:31 -0500

    Add MultiSet

---
+++ src/main/java/io/oreto/latte/collections/MultiSet.java
@@ -0,0 +1,167 @@
+package io.oreto.latte.collections;
+
+import java.util.*;
+
+public class MultiSet<T extends Comparable<T>> implements Iterable<T> {
+    protected static <T extends Comparable<T>> int nullsFirst(T value, Key<T> key) {
+        if (value == key.value) return 0;
+        if (value == null) return -1;
+        if (key.value == null) return 1;
+        return value.compareTo(key.value);
+    }
+    protected static class Key<T extends Comparable<T>> implements Comparable<Key<T>> {
+        private int i;
+        private final T value;
+
+        public Key(T value) {
+            this.i = 1;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(Key<T> o) {
+            return nullsFirst(value, o);
+        }
+    }
+
+    private final TreeSet<Key<T>> tSet;
+    private int size;
+
+    public MultiSet() {
+       this.tSet = new TreeSet<>();
+       this.size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return tSet.isEmpty();
+    }
+
+    public boolean isNotEmpty() {
+        return !tSet.isEmpty();
+    }
+
+    public boolean contains(T t) {
+        for (Key<T> key : tSet) {
+            int i = nullsFirst(t, key);
+            if (i == 0)
+                return true;
+            else if (i < 0)
+                return false;
+        }
+        return false;
+    }
+
+    public MultiSet<T> add(T t) {
+        for (Key<T> key : tSet) {
+            int i = nullsFirst(t, key);
+            if (i == 0) {
+                key.i++;
+                size++;
+                return this;
+            } else if (i < 0)
+                break;
+        }
+        tSet.add(new Key<>(t));
+        size++;
+        return this;
+    }
+
+    @SafeVarargs
+    public final MultiSet<T> addAll(T... collection) {
+        for (T t : collection)
+            add(t);
+        return this;
+    }
+
+    public MultiSet<T> addAll(Collection<T> collection) {
+        for (T t : collection)
+            add(t);
+        return this;
+    }
+
+    public MultiSet<T> remove(T t) {
+        for (Key<T> key : tSet) {
+            int i = nullsFirst(t, key);
+            if (i == 0) {
+                key.i--;
+                if (key.i == 0) {
+                    tSet.remove(key);
+                }
+                size--;
+                return this;
+            } else if (i < 0)
+                break;
+        }
+        return this;
+    }
+
+    @SafeVarargs
+    public final MultiSet<T> removeAll(T... collection) {
+        for (T t : collection)
+            remove(t);
+        return this;
+    }
+
+    public MultiSet<T> removeAll(Collection<T> collection) {
+        for (T t : collection)
+            remove(t);
+        return this;
+    }
+
+    public List<T> toList() {
+        List<T> list = new ArrayList<>();
+        iterator().forEachRemaining(list::add);
+        return list;
+    }
+
+    public T[] toArray(T[] t) {
+        return toList().toArray(t);
+    }
+
+    public MultiSet<T> clear() {
+        tSet.clear();
+        size = 0;
+        return this;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        Iterator<Key<T>> iterator = tSet.iterator();
+
+        if (iterator.hasNext()) {
+            return new Iterator<T>() {
+                Key<T> key = iterator.next();
+                int i = 0;
+
+                @Override
+                public boolean hasNext() {
+                    return i < key.i || iterator.hasNext();
+                }
+                @Override
+                public T next() {
+                    i++;
+                    if (i > key.i) {
+                        key = iterator.next();
+                        i = 1;
+                    }
+                    return key.value;
+                }
+            };
+        } else {
+            return new Iterator<T>() {
+                @Override
+                public boolean hasNext() {
+                    return false;
+                }
+                @Override
+                public T next() {
+                    return iterator.next().value;
+                }
+            };
+        }
+    }
+}
--- src/test/java/io/oreto/latte/MultiMapTest.java
+++ src/test/java/io/oreto/latte/MultiMapTest.java
@@ -1,12 +1,13 @@
 package io.oreto.latte;
 
+import io.oreto.latte.collections.MultiSet;
 import io.oreto.latte.map.MultiInt;
 import io.oreto.latte.map.MultiString;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MultiMapTest {
 
@@ -34,5 +35,21 @@ public class MultiMapTest {
                 , multiString.put("test", "").get("test"));
 
         assertEquals("Michael Ross", multiString.join(" ").get("test"));
+    }
+
+    @Test void MultiSetTest() {
+        MultiSet<Integer> set = new MultiSet<Integer>().addAll(5, 1, 10, -1, -1);
+        assertArrayEquals(new Integer[] { -1, -1, 1, 5, 10 }, set.toArray(new Integer[0]));
+        assertEquals(5, set.size());
+        assertEquals(2, set.removeAll(1, 5, 10).size());
+        assertArrayEquals(new Integer[] { -1, -1 }, set.toArray(new Integer[0]));
+        assertArrayEquals(new Integer[] { -1 }, set.remove(-1).toArray(new Integer[0]));
+        assertTrue(set.clear().isEmpty());
+        assertEquals(0, set.size());
+
+        set.addAll(null, 1, 1, 10, null, 5, null);
+        assertArrayEquals(new Integer[] { null, null, null, 1, 1, 5, 10 }, set.toArray(new Integer[0]));
+        assertArrayEquals(new Integer[] { null, 1, 5, 10 }, set.removeAll(null, null, 1).toArray(new Integer[0]));
+        assertEquals(4, set.size());
     }
 }
